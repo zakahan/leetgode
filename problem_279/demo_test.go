8b0a4d4c781a446103a983bb90ace6ecbde10ad5
hanzhi <jupiteroroz@outlook.com>
2025-01-07 17:57:16 +0800

    占个位置

---
+++ problem_279/demo_test.go
@@ -0,0 +1,13 @@
+// -------------------------------------------------
+// Package problem_279
+// Author: hanzhi
+// Date: 2025/1/7
+// -------------------------------------------------
+
+package problem_279
+
+import "testing"
+
+func TestDemo(t *testing.T) {
+	getSq()
+}
